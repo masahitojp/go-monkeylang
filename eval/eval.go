90ae3d504d3f8e0ae265e71168e6f4c2c2ddb3f0
Jonathan Pentecost <jonathan@karhoo.com>
2017-03-12 11:59:27 +0000

    Can evaluate integers and booleans

--- eval/eval.go
+++ eval/eval.go
@@ -24,6 +24,10 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
@@ -48,6 +52,43 @@ func evalStatements(statements []ast.Statement) object.Object {
 	return result
 }
 
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+
+	if left.Type() == object.INTEGER && right.Type() == object.INTEGER {
+		leftVal := left.(*object.Integer).Value
+		rightVal := right.(*object.Integer).Value
+
+		switch operator {
+		// Return Integers
+		case "+":
+			return &object.Integer{Value: leftVal + rightVal}
+		case "-":
+			return &object.Integer{Value: leftVal - rightVal}
+		case "*":
+			return &object.Integer{Value: leftVal * rightVal}
+		case "/":
+			return &object.Integer{Value: leftVal / rightVal}
+			// Return Boolean
+		case "<":
+			return nativeBoolToBooleanObject(leftVal < rightVal)
+		case ">":
+			return nativeBoolToBooleanObject(leftVal > rightVal)
+		case "==":
+			return nativeBoolToBooleanObject(leftVal == rightVal)
+		case "!=":
+			return nativeBoolToBooleanObject(leftVal != rightVal)
+		default:
+			return NULL
+		}
+	} else if operator == "==" {
+		return nativeBoolToBooleanObject(left == right)
+	} else if operator == "!=" {
+		return nativeBoolToBooleanObject(left != right)
+	} else {
+		return NULL
+	}
+}
+
 func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
@@ -55,6 +96,7 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	case "-":
 		return evalMinusOperatorExpression(right)
 	default:
+		// TODO(): Should this return an error?
 		return NULL
 	}
 }
@@ -78,6 +120,14 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	case NULL:
 		return TRUE
 	default:
+		return FALSE
+	}
+}
+
+func nativeBoolToBooleanObject(truthy bool) object.Object {
+	if truthy {
+		return TRUE
+	} else {
 		return FALSE
 	}
 }
